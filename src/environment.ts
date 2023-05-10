6df84dbf46ab62acd914a7f46e3ec251e32e605e
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2023-05-10 12:50:40 -0500

    last commit

--- src/environment.ts
+++ src/environment.ts
@@ -1,2 +1,4 @@
-export const apiUrl = `https://4e36-200-1-182-131.ngrok-free.app`;
-export const socketUrl = `ws://4e36-200-1-182-131.ngrok-free.app`;
+const domain = `2e2e-200-1-182-131.ngrok-free.app`
+
+export const apiUrl = `https://${domain}`;
+export const socketUrl = `ws://${domain}`;
